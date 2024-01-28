6681558990005b8a704a89a1332ec60b4d9a20e6
Ida Wiklund Hagberg <idawiklundhagberg@gmail.com>
2024-01-28 14:10:21 +0000

    Add method as props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,7 @@ import EventsFunctional from "./components/EventsFunctional";
 import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
 import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
 import NestingComponents from "./components/NestingComponents";
+import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
 
 function App() {
   return (
@@ -37,6 +38,12 @@ function App() {
 function App() {
   return <div className="App">
       <NestingComponents />
+  </div>;
+}
+
+function App() {
+  return <div className="App">
+      <MethodsAsPropsParent />
   </div>;
 }
 
